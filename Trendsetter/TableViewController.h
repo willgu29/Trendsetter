c9e7fba10ce1287d2d3d7d9e58e53988721e20aa
vivekdud <vivekvinodh@guest-wireless-207-151-035-004.usc.edu>
2014-11-08 04:40:36 -0800

    Replaced Table viewcontroller .H

--- Trendsetter/TableViewController.h
+++ Trendsetter/TableViewController.h
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TableViewController : UIViewController
+@interface TableViewController : UIViewController <UITableViewDataSource, UITableViewDelegate>
 
 @end
