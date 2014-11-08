8b7e5e35ace620444c2314420f76f36f43ea7c7a
Will Gu <willgu29@gmail.com>
2014-11-08 06:34:22 -0800

    Added MyIdeas VC and Cell files

---
+++ Trendsetter/MyIdeasTableViewCell.m
@@ -0,0 +1,23 @@
+//
+//  MyIdeasTableViewCell.m
+//  Trendsetter
+//
+//  Created by William Gu on 11/8/14.
+//  Copyright (c) 2014 William Gu. All rights reserved.
+//
+
+#import "MyIdeasTableViewCell.h"
+
+@implementation MyIdeasTableViewCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
