9ed5255deb6d3335143694c9839d101002cc9a1b
Will Gu <willgu29@gmail.com>
2014-11-08 01:11:39 -0800

    First Commit

---
+++ Trendsetter/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  Trendsetter
+//
+//  Created by William Gu on 11/8/14.
+//  Copyright (c) 2014 William Gu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
